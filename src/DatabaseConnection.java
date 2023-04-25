7eccec2839052d8a5c93a7336380c6d4441f437d
StefanMaring <smaring@ziggo.nl>
2023-04-25 15:51:28 +0200

    Working database connection

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,25 +1,26 @@
 import java.sql.*;
 public class DatabaseConnection {
     private static String url = "jdbc:mysql://localhost:3306/hmi";
-    private static String driverName = "com.mysql.jdbc.Driver";
+    private static String driverName = "com.mysql.cj.jdbc.Driver";
     private static String username = "root";
     private static String password = "";
-    private static Connection con;
+    private static Connection conn;
     private static String urlstring;
 
-    public static Connection getConnection() {
+    public Connection getConnection() {
         try {
             Class.forName(driverName);
             try {
-                con = DriverManager.getConnection(urlstring, username, password);
+                conn = DriverManager.getConnection(url, username, password);
             } catch (SQLException ex) {
                 // log an exception. fro example:
                 System.out.println("Failed to create the database connection.");
+                System.out.println(ex.getMessage());
             }
         } catch (ClassNotFoundException ex) {
             // log an exception. for example:
             System.out.println("Driver not found.");
         }
-        return con;
+        return conn;
     }
 }
