9b2487d13c39d52fcb9f1847cf65bdf674c99fd1
Wrymouth <jacco.potters@outlook.com>
2023-04-26 12:36:41 +0200

    rename orderline arraylist

--- src/Order.java
+++ src/Order.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Order {
     private int id;
     private String name;
-    private ArrayList<Orderline> products;
+    private ArrayList<Orderline> orderlines;
 
     public Order(int id, String name) {
         this.id = id;
@@ -19,8 +19,8 @@ public class Order {
         return name;
     }
 
-    public ArrayList<Orderline> getProducts() {
-        return products;
+    public ArrayList<Orderline> getOrderlines() {
+        return orderlines;
     }
 
     public void placeProductsInBoxes() {
