1bd74850465382f63a89f71bed8f3b32e965042d
Veravv <vera.van.veenhuizen@gmail.com>
2023-05-23 15:10:42 +0200

    exception error order wijzigen dialog

--- src/EditOrderDialog.java
+++ src/EditOrderDialog.java
@@ -70,6 +70,13 @@ public class EditOrderDialog extends JDialog implements ActionListener {
             order.addOrderline(orderline);
             pProductList.setOrderlines(order.getOrderlines());
         } else if (e.getSource() == bSave) {
+            ArrayList<OrderlinePanel> panels = pProductList.getOrderlinePanels();
+            for (OrderlinePanel panel : panels){
+                if(panel.isError()){
+                    JOptionPane.showMessageDialog(this, "ongeldige invoer bij: ", "Foutmelding", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+            }
             DBMethods.updateOrder(order, pProductList.getOrderlines());
             setVisible(false);
             dispose();
